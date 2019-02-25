93c4f0153b983605490008cd7429006849f0530f
yiyang lu <emily.lu@ou.edu>
2019-02-24 22:56:23 -0600

    Started Files

---
+++ Desktop/lab6/Todo.java
@@ -0,0 +1,16 @@
+public class Todo {/*
+Lab Section_________: 12
+Last Modified_______: 2019-02-24
+OU ID_______________: 113377227
+OU EMAIL____________: Emily.lu@ou.edu
+
+Tasks:
+1. 
+
+Estimated Time (minutes):
+1.
+
+Actual Time (minutes):
+1. 
+
+*/}
